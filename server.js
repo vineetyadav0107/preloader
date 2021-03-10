beb211d37ef910bf6440db2924dd810130550806
vineetyadav0107 <vineetyadav0107@gmail.com>
2021-03-10 20:24:31 +0530

    vineet

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/preloader'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/preloader'}),
+    res.sendFile('index.html', {root: 'dist/preloader/'}),
 );
 
 // Start the app by listening on the default Heroku port
